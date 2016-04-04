b67a2b4a046ce9d5a101e1e07ce5d98961ab069e
Jun <i2r.jun@gmail.com>
2016-04-04 23:11:57 +0900

    Update config

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
-export const MY_DEVICE_ID = 'cc3200';
+export const MY_DEVICE_ID = 'edison';
 // export const HOST = 'http://edison.idea2r.io/edisons';
-export const HOST = 'stwide.idea2r.io';
-export const PORT = 80;
-export const DEVICES_INBOX_COLLECTION_NAME = 'devices.inbox';
-export const DEVICES_INBOX_SUBSCRIVE_NAME = 'devices.inbox';
-export const DEVICES_COLLECTION_NAME = 'devices';
-export const DEVICES_SUBSCRIBE_NAME = 'device';
+export const HOST = 'localhost';
+export const PORT = 3000;
+export const DEVICES_INBOX_COLLECTION_NAME = 'things.inbox';
+export const DEVICES_INBOX_SUBSCRIVE_NAME = 'myInbox';
+export const DEVICES_COLLECTION_NAME = 'things';
+export const DEVICES_SUBSCRIBE_NAME = 'things';
 export const DEVELOPMENT_MODE = process.env.NODE_ENV === 'development';
 export const LED = {
     BASIC_LED_PROTOCOL_FORMAT: [2, 1, 1, 0x10, 0, 0, 0, 84, 3],
@@ -31,5 +31,5 @@ export function setEnvironments() {
         colorize: true
     });
 
-    // winston.level = 'debug';
+    winston.level = 'debug';
 }
